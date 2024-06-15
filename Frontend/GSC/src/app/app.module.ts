677496ee3f2c3dbfd52c0816009804d51f13764c
Slim Bouzidi <slim.bouzidi@ced.group>
2024-06-15 01:22:22 +0100

    v1 15-06

--- Frontend/GSC/src/app/app.module.ts
+++ Frontend/GSC/src/app/app.module.ts
@@ -3,22 +3,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+import { NavBarComponent } from './NavBar/NavBar.component';
 import { OverviewComponent } from './Overview/Overview.component';
 import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
 @NgModule({
   declarations: [	
     AppComponent,
+      NavBarComponent,
       OverviewComponent
-=======
-import { NavBarComponent } from './NavBar/NavBar.component';
-
-@NgModule({
-  declarations: [	
-    AppComponent,
-      NavBarComponent
->>>>>>> 30491ed1a51f950fbd6232ca6aed4e4cf5036d4c
    ],
   imports: [
     BrowserModule,
