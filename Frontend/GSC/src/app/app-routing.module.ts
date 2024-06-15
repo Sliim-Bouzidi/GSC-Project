74634f2e5c4885b2467176530228c0ef0dadc367
RayenEsen <rayentriki2003@gmail.com>
2024-06-16 00:01:52 +0100

    "v1 15-06"

--- Frontend/GSC/src/app/app-routing.module.ts
+++ Frontend/GSC/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './Overview/Overview.component'; 
-
+import { MonthComponent } from './Month/Month.component';
 const routes: Routes = [
-  { path: '', component: OverviewComponent }
-  
+  { path: 'Clients/:id', component: OverviewComponent },
+  { path: '', component: MonthComponent }
 ];
 
 @NgModule({
